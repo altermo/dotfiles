0c545e121595db770b5bb226eb6a681a4ac07db4
altermo <107814000+altermo@users.noreply.github.com>
2024-03-17 16:07:32 +0100

    D  bin/blender;M  bin/data_select;A  bin/pwofi;M  fish/config.fish;M  hypr/hyprland.conf;D  hypr/pyprland.toml

--- fish/config.fish
+++ fish/config.fish
@@ -20,6 +20,7 @@ set -x PAGER 'bat --decorations never --paging=always --pager="less --SILENT -RF
 set -x MANPAGER "$PAGER -l man"
 set -x READ_QUICKLY_RATE 350
 set -x PYTHONPATH "$HOME/.venv/lib/python3.11/site-packages"
+set -x GTK_THEME Adwaita:dark
 set -U fish_user_paths $HOME/.local/bin $HOME/.cargo/bin $HOME/.nix-profile/bin/
 set -p fish_complete_path ~/.config/fish/outer_completions
 set fish_cursor_insert      line
@@ -28,13 +29,11 @@ set fish_cursor_replace     underscore
 set fish_cursor_visual      block
 fish_vi_key_bindings
 
-#hooks
+#sources/hooks
 if invim
     function __hook_nvim_lcd -v PWD;nvr -c "silent! lcd $PWD" &;end
     nvr -c "silent! lcd $PWD" &
 end
-
-#source
 if not test -f ~/.config/fish/carapace/carapace.fish
     mkdir -p ~/.config/fish/carapace
     carapace --list|awk '{print $1}'|xargs -I{} touch ~/.config/fish/carapace/{}.fish
@@ -46,7 +45,7 @@ zoxide init fish|source
 #fzf
 function ff;plocate (dirname $PWD/.) $argv|fzf|read out&&test -d $out&&cd $out||begin;test -f $out&&$EDITOR $out;end;end
 function ffuncs;functions -a|fzf --preview="fish -ic 'type {1}|bat -pp -l fish --color=always'";end
-function fc;command ls ~/.config|fzf|read out&&test $out&&ranger ~/.config/$out;end
+function fc;env ls ~/.config|fzf|read out&&test $out&&ranger ~/.config/$out;end
 function ft;tldr -l|fzf --preview 'tldr --color=always {}'|xargs -r tldr;end
 
 #installer
@@ -72,22 +71,19 @@ abbr gp "git push"
 abbr gca "git commit -a -m"
 abbr ga "git commit -a --amend"
 abbr gt "git checkout"
-abbr gmd "git merge development"
 abbr gs "git status"
 abbr gd "git diff"
-abbr gi "gh gist"
-abbr gb "git bisect" #Find buged commit
 
 #optinos
-function cal;command cal -wm --color=always $argv|lolcat;end
+function cal;env cal -wm --color=always $argv|lolcat;end
 alias rm 'rm -I'
 alias cp 'cp -rib'
 alias xcp 'xcp -rn'
 alias mv 'mv -ib'
 alias ln 'ln -ibs'
 alias mkdir 'mkdir -p'
-alias touch 'mkdir -p (dirname $argv)&&command touch'
-alias neofetch 'clear;command fastfetch'
+alias touch 'mkdir -p (dirname $argv)&&env touch'
+alias neofetch 'clear;fastfetch'
 alias wget 'wget -c'
 alias fd 'fd -H'
 alias zip 'zip -r -v'
@@ -104,25 +100,22 @@ abbr - z\ -
 abbr t tldr
 abbr c clear
 abbr r ranger
-abbr rr "command ranger"
 abbr cr touch
 abbr mkd mkdir
 abbr pow acpi
-abbr com command
 abbr v nvim
 abbr g grep
 abbr img2txt tesseract
 abbr wifi nmtui-connect
 abbr list_ports 'lsof -i'
-abbr copy 'xsel -b'
 abbr unmount umount
 ##other
-alias cargob "watchexec cargo check"
-alias cargoc cargo\ clippy
+abbr cargob "watchexec cargo check"
+abbr cargoc cargo\ clippy
 #alias rich "python -m rich"
-abbr date 'date +"  %H:%M:%S  %Y/%m/%d;%V"'
-alias ls 'exa -aF'
-alias l 'exa -F'
+#abbr date 'date +"  %H:%M:%S  %Y/%m/%d;%V"'
+alias ls 'eza -aF'
+alias l 'eza -F'
 alias ll 'l -lh --git'
 alias la 'ls -lh --git'
 abbr cp xcp
@@ -132,47 +125,39 @@ alias cat "bat -pp"
 alias pip ~/.venv/bin/pip
 abbr cd z
 abbr vim nvim
-abbr tree "exa -T"
+abbr tree "eza -T"
 abbr find fd
 abbr df duf
 abbr du dua
 abbr sudo doas
 
 #vim
-function riv;invim&&sh -c $argv[2]&&kill (cut -f 6 -d " " /proc/$fish_pid/stat)||command $argv[1] $argv[3..];end
+function riv;invim&&sh -c $argv[2]&&kill (cut -f 6 -d " " /proc/$fish_pid/stat)||env $argv[1] $argv[3..];end
 function nvim
     if string match -q -r -- '^[-+]' $argv
-        command nvim $argv
+        env nvim $argv
         return
     end
     riv nvim "[ $argv ]&&nvr $argv||nvr ." $argv
 end
 function ntime;time $argv[1] +"autocmd VimEnter * quit" $argv[2..];end
 alias kpn 'pkill -9 -P 1 "^nvim\$";pkill -9 -P 1 -f language_server_linux_x64'
-alias vimacs 'NVIM_APPNAME=vimacs command nvim'
-alias nvchad 'NVIM_APPNAME=NvChad command nvim'
-alias spvim 'NVIM_APPNAME=SpaceVim command nvim'
+function nvims;echo vimacs\nNvChad\nSpaceVim|fzf|read out&&test -n "$out"&&NVIM_APPNAME=$out env nvim;end
 
 #emacs
 alias emacs "setsid emacsclient -c -a 'emacs'"
 alias ec "setsid emacsclient >/dev/null"
-alias rem "killall emacs;command emacs --daemon"
-alias umacs "command emacs --init-directory=/home/user/.config/emacs/"
+alias rem "killall emacs;env emacs --daemon"
+alias umacs "env emacs --init-directory=/home/user/.config/emacs/"
 
 #other
 function ranger;riv ranger "nvr -c 'lua require\"small.ranger\".run(\"$argv\")'" $argv;end
 abbr dtmp 'cd (mktemp -d -p /tmp/user)'
-function rtmp;ranger /tmp;end
-function rtmpu;ranger /tmp/user;end
+abbr ctu 'cd /tmp/user'
 for i in $langs;for j in $langs
     alias "tr$i$j" "trans -b $i:$j"
 end;end
 alias clock 'termdown -z -Z "%H : %M : %S"'
-alias mousefast 'xinput set-prop "AlpsPS/2 ALPS GlidePoint" "libinput Accel Speed" 0.5'
-alias mouseslow 'xinput set-prop "AlpsPS/2 ALPS GlidePoint" "libinput Accel Speed" 0'
-alias mousesnail 'xinput set-prop "AlpsPS/2 ALPS GlidePoint" "libinput Accel Speed" -0.5'
-alias mousewritemove 'xinput set-prop "AlpsPS/2 ALPS GlidePoint" libinput\ Disable\ While\ Typing\ Enabled false'
-alias mousehide 'xbanish -a'
 alias mousetty 'sudo systemctl start gpm.service'
 function clearfuncs;for i in (functions -a);functions -e $i;end;end
 function countdown
@@ -187,11 +172,11 @@ alias nothing "curl -s -L https://raw.githubusercontent.com/keroserene/rickrollr
 alias mkc 'mkdir $argv;cd'
 function qunzip;unzip $argv.zip&&cat $argv&&shred -uvz $argv;end
 alias saferm 'shred -uvz'
-function encrypt;command zip -r --encrypt $argv.zip $argv;end
+function encrypt;env zip -r --encrypt $argv.zip $argv;end
 alias ip "/bin/ip addr | awk '/inet / {print \$2}'"
 alias lightup 'brightnessctl set 10+%'
 alias lightdown 'brightnessctl set 10-%'
-alias beepoff 'sudo rmmod pcspkr.ko.zst'
+#alias beepoff 'sudo rmmod pcspkr.ko.zst'
 function usercreator
     set web (curl "https://randomuser.me/api/?password=upper,lower,special,number,16-20&seed=$argv"(date +%Y50%m01%d) 2>/dev/null)
     echo $web|jq .results[0].login.username -r
@@ -202,7 +187,6 @@ function update_hosts
     begin;curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts;
         cat /etc/hosts.own;end|sudo tee /etc/hosts >/dev/null
 end
-alias blanket "command setsid blanket -h"
 function lnq;ln $argv (basename $argv);end
 alias wifilist 'nmcli device wifi list'
 function fsetsid;setsid fish -ic "$argv";end
@@ -225,21 +209,19 @@ function encsend
     rm -fr /tmp/enc-out.zip
 end
 function decget;unzip (curl "$argv"|psub);end
-alias exe "command ls -p|grep -v /|fzf|xargs -r chmod u+x"
+function exe;test -n "$argv"&&chmod u+x $argv||env ls -p|grep -v /|fzf|xargs -r chmod u+x;end
 alias tsh "sudo systemd-nspawn -D $HOME/.os /sbin/init"
-#alias wm "exec sx qtile start"
 alias wm "exec Hyprland"
 alias vimtip "curl -s -m 3 https://vtip.43z.one"
 alias nvimtip "curl https://www.vimiscool.tech/neotip"
 abbr icargo evcxr
-alias weather "curl wttr.in/\?nFQ"
+abbr weather "curl wttr.in/\?nFQ"
 
 #installer
 if not type fisher >/dev/null 2>&1
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 end
-set _plugins jorgebucaran/fisher nickeb96/puffer-fish andreiborisov/sponge ilancosman/tide
-for i in $_plugins
+for i in jorgebucaran/fisher nickeb96/puffer-fish andreiborisov/sponge ilancosman/tide
     cat ~/.config/fish/fish_plugins|string match $i >/dev/null||fisher install $i
 end
 # vim:fen:
