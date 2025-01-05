374cd60eab7116c4eb57a6f9c60fa8fffac5b496
altermo <107814000+altermo@users.noreply.github.com>
2025-01-05 17:06:04 +0100

    M bin/colors; M bin/fsf; A bin/mpvl; M bin/smart_wkill; M data/configs; M data/links; M fish/config.fish;D  fish/fish_plugins; M hypr/hyprland.conf; M kitty/kitty.conf;D  ranger/rc.conf; A yazi/yazi.toml

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,20 @@
-#preload
+# ;; preload
 if not status --is-interactive;exit;end
 if tty>/dev/null&&test (math (date +%s) - (stat -c %Y /tmp/gh.not 2>/dev/null||echo 0)) -gt 60
-    setsid sh -c 'nm-online -q||exit
+    setsid sh -c 'timeout 1 ping github.com >/dev/null 2>&1 -c 1||exit
     notif=$(gh api notifications|jq "length")
     test 0 = "$notif"||echo -e "\e[7m\ngithub: you have received $notif notifications\e[0m"'&
     disown
     touch /tmp/gh.not
 end
 
-#vars
+# ;; vars
 alias invim 'not [ $INSIDE_EMACS ]&&[ $NVIM ]'
 test "$TEMPFILE"||set -U TEMPFILE /tmp/user/temp.lua
 test -d /tmp/user||mkdir /tmp/user
 set fish_greeting
 set langs 'en' 'sv' 'hu'
-set FILEMANAGER ranger
+set FILEMANAGER yazi
 set -x EDITOR nvim
 set -x VISUAL nvim
 set -x PAGER 'bat --decorations never --paging=always --pager="less --SILENT -RF"'
@@ -28,7 +28,7 @@ set fish_cursor_replace     underscore
 set fish_cursor_visual      block
 fish_vi_key_bindings
 
-#sources/hooks
+# ;; sources/hooks
 if invim
     function __hook_nvim_lcd -v PWD;nvr -c "silent! lcd $PWD" &;end
     nvr -c "silent! lcd $PWD" &
@@ -41,7 +41,7 @@ set -p fish_complete_path ~/.config/fish/carapace
 carapace _carapace|source
 zoxide init fish|source
 
-#paru
+# ;; paru
 alias paru_update "nm-online >/dev/null&&paru -Syu --devel"
 alias paru_clear 'test "$(paru -Qtdq)"&&paru -Qtdq | paru -Rns -'
 alias paru_loop_msg 'paru -Qqd | paru -Rsu --print -'
@@ -57,7 +57,7 @@ alias pal "paru -Ql"
 alias paC "paru -Sc"
 alias mirrorlist_update "curl -s 'https://archlinux.org/mirrorlist/?country=NO&country=SE&country=DK&country=FI&country=DE&country=PL&protocol=https&use_mirror_status=on' | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 10 -|sudo tee /etc/pacman.d/mirrorlist"
 
-#git
+# ;; git
 abbr gCA "git commit -a -m (git status --porcelain|string join ';')"
 abbr gc "git clone"
 abbr gp "git push"
@@ -68,31 +68,31 @@ abbr gs "git status"
 abbr gd "git diff"
 abbr gsa "git stash push"
 abbr gsr "git stash pop"
+abbr gaa "git add -A -N"
 
-#optinos
+# ;; optinos
 alias rm 'rm -I'
 alias cp 'cp -rib'
 alias xcp 'xcp -rn'
 alias mv 'mv -ib'
 alias ln 'ln -ibs'
 alias mkdir 'mkdir -p'
-alias touch 'mkdir -p (dirname $argv)&&env touch'
 alias wget 'wget -c'
 alias fd 'fd -H'
 alias zip 'zip -r -v'
 alias termdown 'termdown -B'
 alias clear 'TERM=xterm env clear'
-alias lolcat 'dotacat -F 0.05'
 alias df 'df -h --output=source,fstype,size,used,pcent,avail,target'
 
-#namig
-##shorter names
+# ;; namig
+## spell mistake
 abbr :q exit
 abbr :Q exit
 abbr z.. z\ ..
 abbr z/ z\ /
+abbr unmount umount
+## smaller name
 abbr - z\ -
-abbr t tldr
 abbr c clear
 abbr r $FILEMANAGER
 abbr cr touch
@@ -101,115 +101,49 @@ abbr pow acpi
 abbr v nvim
 abbr g grep
 abbr wifi nmtui-connect
-abbr list_ports 'lsof -i'
-abbr unmount umount
-##other
+## use other
 alias ed "nvim --clean -E"
-#abbr cargob "watchexec cargo check"
-#abbr cargoc cargo\ clippy
-#alias rich "python -m rich"
-#abbr date 'date +"  %H:%M:%S  %Y/%m/%d;%V"'
 alias ls 'eza -aF'
 alias l 'eza -F'
 alias ll 'l -lh --git'
 alias la 'ls -lh --git'
-abbr cp xcp
 alias more "$PAGER"
 alias less "$PAGER"
 alias cat "bat -pp"
+alias lolcat 'dotacat -F 0.05'
 alias pip ~/.venv/bin/pip
+abbr cp xcp
 abbr cd z
 abbr tree "eza -T"
 abbr find fd
 abbr du dua
 abbr sudo doas
 
-#vim
-function riv;invim&&sh -c $argv[2]&&kill (cut -f 6 -d " " /proc/$fish_pid/stat)||env $argv[1] $argv[3..];end
+# ;; nvim
 function nvim
-    if string match -q -r -- '^[-+]' $argv
+    if string match -q -r -- '^[-+]' $argv;or not invim
         env nvim $argv
         return
     end
-    riv nvim "[ $argv ]&&nvr $argv||nvr ." $argv
+    [ $argv ]&&nvr $argv||nvr .
+    kill (cut -f 6 -d " " /proc/$fish_pid/stat)
 end
-function ntime;time $argv[1] +"autocmd VimEnter * quit" $argv[2..];end
-alias kpn 'pkill -9 -P 1 "^nvim\$";pkill -9 -P 1 -f language_server_linux_x64'
-function nvims;echo vimacs\nNvChad\nSpaceVim\nLazyVim|fzf|read out&&test -n "$out"&&NVIM_APPNAME=$out env nvim;end
-
-#emacs
-alias emacs "setsid emacsclient -c -a 'emacs'"
-alias ec "setsid emacsclient >/dev/null"
-alias rem "killall emacs;env emacs --daemon"
-alias temacs  "/home/user/.nelisp/emacs/src/temacs -Q"
-alias nemacs  "nvim --clean -u /home/user/.nelisp/nelisp/save/setup.lua"
-
-#other
-abbr dtmp 'cd (mktemp -d -p /tmp/user)'
-abbr ctu 'cd /tmp/user'
-for i in $langs;for j in $langs
-    if test $i != $j
-        alias "tra$i$j" "trans -b $i:$j (read)"
-    end
-end;end
-alias clock 'termdown -z -Z "%H : %M : %S"'
-# alias mousetty 'sudo systemctl start gpm.service'
-function clearfuncs;for i in (functions -a);functions -e $i;end;end
-function countdown
-    set save (hyprctl activeworkspace -j|jq .id)
-    termdown $argv
-    hyprctl dispatch workspace $save
-end
-alias tu "HOME=(mktemp -d)"
-alias tb "curl -F file=@- 0x0.st"
-alias nothing "curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash"
-alias mkc 'mkdir $argv;cd'
-function qunzip;unzip $argv.zip&&cat $argv&&shred -uvz $argv;end
-alias saferm 'shred -uvz'
-function encrypt;env zip -r --encrypt $argv.zip $argv;end
-alias ip "/bin/ip addr | awk '/inet / {print \$2}'"
-alias lightup 'brightnessctl set 10+%'
-alias lightdown 'brightnessctl set 10-%'
-#alias beepoff 'sudo rmmod pcspkr.ko.zst'
-function usercreator
-    set web (curl "https://randomuser.me/api/?password=upper,lower,special,number,16-20&seed=$argv"(date +%Y50%m01%d) 2>/dev/null)
-    echo $web|jq .results[0].login.username -r
-    echo $web|jq .results[0].login.password -r
-end
-alias reset 'exec fish -C "$(status current-commandline|string split \;|tail +2)"'
-function update_hosts
-    begin;curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts;
-        cat /etc/hosts.own;end|sudo tee /etc/hosts >/dev/null
-end
-function lnq;ln $argv (basename $argv);end
-function gis
-    pushd .
-    cd ~/.etc/.other
-    fish main.fish 2>/dev/null
-    for i in .mozilla .config .config/nvim .tmp/lua/_later/ .config/dotfiles .qscript .gtd .etc .tmp/lua/vim-plugin-list/
-        cd ~/$i
-        if test "$(git status --porcelain)"
-            echo $i
-            git status --porcelain
+function nvims
+    echo NvChad\nLazyVim|fzf|read out||return 1
+    set conf ~/.config/$out
+    if not test -d $conf
+        switch $out
+            case 'NvChad'
+                git clone https://github.com/NvChad/starter $conf
+                rm -rf $conf/.git
+            case 'LazyVim'
+                git clone https://github.com/LazyVim/starter $conf
+                rm -rf $conf/.git
         end
     end
-    popd
+    NVIM_APPNAME=$out env nvim
 end
-function encsend
-    zip /tmp/enc-out.zip $argv --encrypt
-    /bin/cat /tmp/enc-out.zip|curl -F file=@- -F expires=1 0x0.st
-    rm -f /tmp/enc-out.zip
-end
-function decget;unzip (curl "$argv"|psub);end
-function exe;test -n "$argv"&&chmod u+x $argv||env ls -p|grep -v /|fzf|xargs -r chmod u+x;end
-# alias tsh "sudo systemd-nspawn -D $HOME/.os /sbin/init"
-alias wm "exec Hyprland"
-# alias vimtip "curl -s -m 3 https://vtip.43z.one"
-# alias nvimtip "curl https://www.vimiscool.tech/neotip"
-# abbr icargo evcxr
-abbr weather "curl wttr.in/\?nFQ"
-abbr scan_open_ports "nmap -p- 127.0.0.1"
-function nprog
+function nvim_build_prog
     pushd .
     cd /tmp
     while true
@@ -225,29 +159,67 @@ function nprog
     echo
     popd
 end
-function chat
-    hey -c
-    printf "\e[A"
-    hey -- (read)
-end
-function chatt
-    hey -c
-    printf "\e[A"
-    set a (read)
-    while test -n "$a"
-        hey -- $a
-        printf "\e[A"
-        set a (read)
+
+# ;; other
+abbr dtmp 'cd (mktemp -d -p /tmp/user)'
+alias touch 'mkdir -p (dirname $argv)&&env touch'
+for i in $langs;for j in $langs
+    if test $i != $j
+        alias "tra$i$j" "trans -b $i:$j (read)"
     end
+end;end
+alias clock 'termdown -z -Z "%H : %M : %S"'
+function countdown
+    set save (hyprctl activeworkspace -j|jq .id)
+    termdown $argv
+    hyprctl dispatch workspace $save
 end
-function ffuncs;functions -a|fzf --preview="fish -ic 'type {1}|bat -pp -l fish --color=always'";end
+alias tu "HOME=(mktemp -d)"
+alias tb "curl -F file=@- 0x0.st"
+alias saferm 'shred -uvz'
+function encrypt;env zip -r --encrypt $argv.zip $argv;end
+alias ip "/bin/ip addr | awk '/inet / {print \$2}'"
+alias lightup 'brightnessctl set 10+%'
+alias lightdown 'brightnessctl set 10-%'
+function lnq;ln $argv (basename $argv);end
+function gis
+    pushd .
+    cd ~/.etc/.other
+    fish main.fish 2>/dev/null
+    for i in .mozilla .config .config/nvim .tmp/lua/_later/ .config/dotfiles .qscript .gtd .etc .tmp/lua/vim-plugin-list/
+        cd ~/$i
+        if test "$(git status --porcelain)"
+            echo $i
+            git status --porcelain
+        end
+    end
+    popd
+end
+function exe;test -n "$argv"&&chmod u+x $argv||env ls -p|grep -v /|fzf|xargs -r chmod u+x;end
+alias wm "exec Hyprland"
+abbr weather "curl wttr.in/\?nFQ"
+# function chat
+#     hey -c
+#     printf "\e[A"
+#     hey -- (read)
+# end
+# function chatt
+#     hey -c
+#     printf "\e[A"
+#     set a (read)
+#     while test -n "$a"
+#         hey -- $a
+#         printf "\e[A"
+#         set a (read)
+#     end
+# end
 function cal;env cal -wm --color=always $argv|lolcat;end
 alias neofetch 'clear;fastfetch|lolcat'
-
-#installer
-if not type fisher >/dev/null 2>&1
-    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+if not functions -q tide
+    set -l _tide_tmp_dir (command mktemp -d)
+    curl https://codeload.github.com/ilancosman/tide/tar.gz/v6 | tar -xzC $_tide_tmp_dir
+    command cp -R $_tide_tmp_dir/*/{completions,conf.d,functions} $__fish_config_dir
+    emit _tide_init_install
 end
-for i in jorgebucaran/fisher nickeb96/puffer-fish andreiborisov/sponge ilancosman/tide
-    cat ~/.config/fish/fish_plugins|string match $i >/dev/null||fisher install $i
-end
+alias temacs "/home/user/.nelisp/emacs/src/temacs -Q"
+alias nemacs "nvim --clean -u /home/user/.nelisp/nelisp/save/setup.lua"
