2abcc3ad839b777b115c9184dbd43db765aec1bf
altermo <107814000+altermo@users.noreply.github.com>
2025-01-12 09:55:35 +0100

    M bin/gnvim.sh; M kitty/kitty.conf

--- bin/gnvim.sh
+++ bin/gnvim.sh
@@ -8,5 +8,5 @@ exec kitty -o window_padding_width=0\
      -o map="ctrl+= change_font_size all +1.0"\
      -o map="ctrl+- change_font_size all -1.0"\
      -o map="ctrl+0 change_font_size all 0"\
-     -o font_size=12\
+     -o font_size=14\
      -e nvim "$@"
