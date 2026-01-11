4c11b51847eb1388fae3a0b4400b511df381ba18
altermo <107814000+altermo@users.noreply.github.com>
2026-01-11 13:50:10 +0100

    new NEOVIM config

--- nvim/queries/lua/highlights.scm
+++
@@ -1,9 +0,0 @@
-;; extends
-
-(("function" @keyword.function) (#set! conceal "λ"))
-(("local" @keyword) (#set! conceal "»"))
-(("then" @conditional) (#set! conceal "⇒"))
-(("do" @repeat) (#set! conceal "⇒"))
-(("end" @keyword.function) (#set! conceal "⇐"))
-
-((binary_expression (unary_expression "not") "==") @comment.todo @markup.strikethrough @markup.underline)
